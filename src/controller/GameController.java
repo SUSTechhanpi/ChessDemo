07bfa814c97a8b58596d8357df7fa164e859417f
SUSTechuser <12111309@mail.sustech.edu.cn>
2022-05-17 16:57:15 +0800

    第一版

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -5,6 +5,7 @@ import view.Chessboard;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameController {
@@ -22,7 +23,7 @@ public class GameController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return new ArrayList<>();
     }
 
 }
